5c3ddf290116d93f21f4b8237641ca87e7ab4cbd
Praveen Kumar <115071711+Praveenkumar07007@users.noreply.github.com>
2025-01-25 00:37:05 +0530

    remove outdated Docker run instructions and add documentation on Docker volumes

--- node-bind-mount/index.js
+++ node-bind-mount/index.js
@@ -7,9 +7,6 @@ app.get('/home', (req, res) => {
   res.send("This is home page")
 })
 
-// for mapping the local host and docker file changes
-// docker run -it --init -p 3002:3000 -v "%cd%":/prave/nodejs/node-bind-mound-project app-bind-mount-node:latest
-// use this cammand to run the docker container
 app.get('/info', (req, res) => {
   res.send("This is info page")
 })
